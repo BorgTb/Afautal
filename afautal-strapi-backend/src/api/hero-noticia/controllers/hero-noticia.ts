e2b85b177ef088ea0f9dc58923d1c329abe50ba4
BorgTb <82419792+BorgTb@users.noreply.github.com>
2026-03-20 17:11:52 -0300

    sas

---
+++ afautal-strapi-backend/src/api/hero-noticia/controllers/hero-noticia.ts
@@ -0,0 +1,7 @@
+/**
+ * hero-noticia controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::hero-noticia.hero-noticia');
---
+++ afautal-strapi-backend/src/api/hero-noticia/routes/hero-noticia.ts
@@ -0,0 +1,7 @@
+/**
+ * hero-noticia router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::hero-noticia.hero-noticia');
