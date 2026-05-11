008f63e6c078f584f66d4e1059b5eab7943184d2
BorgTb <ameza21@alumnos.utalca.cl>
2026-05-11 10:17:43 -0400

    test

--- afautal-strapi-backend/config/admin.ts
+++ afautal-strapi-backend/config/admin.ts
@@ -24,10 +24,13 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Admin => 
     config: {
       handler(uid: string, { documentId }: { documentId: string }) {
         if (uid === 'api::noticia.noticia') {
-          const baseUrl = env('FRONTEND_URL', 'http://localhost:3000');
-          // Asegurarse de no duplicar http:// si ya viene en la variable
-          const formattedUrl = baseUrl.startsWith('http') ? baseUrl : `http://${baseUrl}`;
-          return `${formattedUrl}/api/preview?secret=${env('PREVIEW_SECRET', 'tu-secreto-super-seguro')}&documentId=${documentId}`;
+          // Obtenemos la URL y limpiamos cualquier barra final que pueda traer
+          let baseUrl = env('FRONTEND_URL', 'http://localhost:3000').replace(/\/$/, "");
+          
+          const secret = env('PREVIEW_SECRET', 'tu-secreto-super-seguro');
+          
+          // Retornamos la URL limpia
+          return `${baseUrl}/api/preview?secret=${secret}&documentId=${documentId}`;
         }
         return null;
       },
